d1b8a5d960659206ae61b41bf595eef765631ab1
rnort003 <rnort003@plattsburgh.edu>
2019-08-06 15:11:08 -0400

    Create configpy.py

---
+++ configpy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+bones = {'host': 'smtp.gmail.com',
+         'user': 'exampleuser@gmail.com',
+         'passwd': 'examplepassword',
+         'port': '587',
+         'SC_email': 'servicecenter@company.com',
+         'SS_email': 'supportservices@company.com',
+         'ITSEC_email': 'itsecurity@company.com'}
+use_anonymous = True
